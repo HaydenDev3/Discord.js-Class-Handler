2028c4a83a7318282f29f4c17fc6829ed9c12760
Unbreakablenight <66907684+Unbreakablenight@users.noreply.github.com>
2022-10-22 09:22:55 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,7 @@
 require('dotenv').config();
+const { ExtendedClient } = require('discord.js');
 
-const { Collection, Client, default: Discord } = require('discord.js');
-const TOKEN = process.env.TOKEN ?? null;
-const { glob } = require("glob");
-const { promisify } = require("util");
-
-const globPromise = promisify(glob);
-
-class UnbreakableBot {
-    constructor() {
-        this.bot = new Client({
-            intents: 14023, // Multipurpose intents.
-            shards: "auto",
-            presence: {
-                    status: "dnd",
-                    activity: {
-                    type: "PLAYING",
-                    text: "www.unbreakable.tk"
-                }
-            },
-        });
-
-        this.bot.commands = new Collection();
-        this.bot.slashCommands = [];
-    };
-
-    async registerCommands () {
-        const slashCommands = await globPromise(
-            `${process.cwd()}/commands/*/*.js`
-        );
-    
-        // const arrayOfSlashCommands = [];
-        slashCommands.map((value) => {
-            const file = require(value);
-            if (!file?.name) return;
-            this.bot.commands.set(file.name, file);
-    
-            if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
-            this.bot.slashCommands.push(file);
-        });
-    };
-
-    async registerEvents() {
-        const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
-        const bot = this.bot;
-
-        eventFiles.map((value) => require(value)(bot) ?? require(value));
-    };
-
-    async start () {
-        await this.bot.login( TOKEN ).then(() => console.log(`Logged in.`));
-    };
-};
-
-const bot = new UnbreakableBot();
+const bot = new ExtendedClient();
 
 try {
     bot.registerCommands(); // Registering our Slash Commands.
